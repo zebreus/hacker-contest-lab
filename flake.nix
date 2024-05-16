41871350ad6f9f7a374b48fea6613fa69cbdfb8d
Zebreus <lennart@zebre.us>
2024-05-16 10:55:16 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Pentest lab stuff";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:zebreus/nixpkgs/fix-autopsy";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        rec {
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          name = "hacker-contest-qualification";
+          packages.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              python3
+              gnumake
+              nixpkgs-fmt
+              nil
+              jq
+              curl
+              autopsy
+              perl
+              sleuthkit
+              testdisk
+              imagemagick
+              nmap
+              bash
+            ];
+          };
+        }
+      );
+}
